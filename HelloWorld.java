e51ed6509cfd35ac365f1a1976dc07881642366a
Sukhada Garud <>
2025-09-24 11:56:31 +0100

    new pull request

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! my new project ");
+    System.out.println("Hello world! my new project and new pull request");
   }
 }
